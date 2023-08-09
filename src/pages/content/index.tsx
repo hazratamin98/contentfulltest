79979508e54e91c80c161827bb34d816ec85db99
Shahroon Farooqi <84621767+hazratamin98@users.noreply.github.com>
2023-08-10 00:26:49 +0500

    done for today

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import client from "@/contents";
+import Image from "next/image";
 
 export async function getStaticProps() {
   // Fetch content from Contentful
-  const entries = await client.getEntries({ content_type: "Blog" });
+  const entries = await client.getEntries({ content_type: "blog" });
 
   return {
     props: {
-      content: entries,
+      content: entries.items.map((item: any) => item.fields),
     },
   };
 }
@@ -15,15 +16,24 @@ export async function getStaticProps() {
 const ContentPage = ({ content }: any) => {
   console.log(content);
   return (
-    <></>
-    // <div>
-    //   {content.map((item: any, index: any) => (
-    //     <div key={index}>
-    //       <h1>{item.title}</h1>
-    //       {/* Render other content fields here */}
-    //     </div>
-    //   ))}
-    // </div>
+    <div>
+      {content?.map((item: any, index: any) => (
+        <div key={index}>
+          <h1>{item.title}</h1>
+          <h1>{item.description}</h1>
+          <h1>{item.isFeatured}</h1>
+          <h1>{item.publishedData}</h1>
+          <Image
+            src={item.heroImage.fields.file}
+            alt="user profile picture"
+            width={300}
+            height={300}
+          />
+
+          {/* Render other content fields here */}
+        </div>
+      ))}
+    </div>
   );
 };
 
