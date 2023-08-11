89e371991965f2962b7b3e4b83427ee88642cb7d
Shahroon Farooqi <84621767+hazratamin98@users.noreply.github.com>
2023-08-11 19:51:41 +0500

    filtered data

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import client from "@/contents";
 import Image from "next/image";
 
 export async function getStaticProps() {
   // Fetch content from Contentful
-  const entries = await client.getEntries({ content_type: "blog" });
+  const entries = await client.getEntries({
+    content_type: "blog",
+    tag: "E-Unicycles",
+  });
 
   return {
     props: {
@@ -15,22 +18,49 @@ export async function getStaticProps() {
 
 const ContentPage = ({ content }: any) => {
   console.log(content);
+
+  const [filteredProducts, setFilteredProducts] = useState(content);
+
+  // const handleProductTypeFilter = (productType: string) => {
+  //   if (productType === "all") {
+  //     setFilteredProducts(content);
+  //   } else {
+  //     const filtered = content.filter(
+  //       (product: any) => product.tag.fields.title === productType
+  //     );
+  //     setFilteredProducts(filtered);
+  //   }
+  // };
+
   return (
     <div>
+      {/* <div>
+        <button onClick={() => handleProductTypeFilter("all")}>All</button>{" "}
+        <br />
+        <button onClick={() => handleProductTypeFilter("E-Unicycles")}>
+          E-Unicycles
+        </button>{" "}
+        <br />
+        <button onClick={() => handleProductTypeFilter("E-bikes")}>
+          E-bikes
+        </button>
+      </div> */}
       {content?.map((item: any, index: any) => (
         <div key={index}>
           <h1>{item.title}</h1>
-          <h1>{item.description}</h1>
-          <h1>{item.isFeatured}</h1>
-          <h1>{item.publishedData}</h1>
+          <h1>{item.publishedDate}</h1>
+          {/* Other fields */}
+          <h1>{item.tag.fields.title}</h1>
+          {/* Rest of the fields */}
           <Image
-            src={item.heroImage.fields.file}
+            src={item.author.map(
+              (author: any) => author.fields.authorImage.fields.file.url
+            )}
             alt="user profile picture"
             width={300}
             height={300}
           />
-
-          {/* Render other content fields here */}
+          a{/* Render other content fields here */}
         </div>
       ))}
     </div>
